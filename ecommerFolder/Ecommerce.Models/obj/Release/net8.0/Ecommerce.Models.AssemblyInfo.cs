d9d83310153e80555da4811f5ac37f701e923444
eiuThanhDuy <duynguyen120504@gmail.com>
2024-12-09 18:18:16 +0700

    a
    a

--- ecommerFolder/Ecommerce.Models/obj/Release/net8.0/Ecommerce.Models.AssemblyInfo.cs
+++ ecommerFolder/Ecommerce.Models/obj/Release/net8.0/Ecommerce.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e46655dcf1cc9146d82bd9e34f9daee0ce0cf56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
